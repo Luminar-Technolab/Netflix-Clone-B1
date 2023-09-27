43d8451ca08efab1486aaebc6926d6a455d3ba0b
soniagkalliath <soniageorge111@gmail.com>
2023-09-27 11:36:20 +0530

    Netflix design

---
+++ src/components/Banner.jsx
@@ -0,0 +1,35 @@
+import React,{useState, useEffect} from 'react';
+import tmdbAxiosInstance from '../tmdbAxiosInstance';
+import './Banner.css';
+
+function Banner({fetchUrl}) {
+    const [movie,setMovie] = useState({})
+    const base_url = "https://image.tmdb.org/t/p/original/";
+    const fetchData = async ()=>{
+      const {data} = await tmdbAxiosInstance.get(fetchUrl)
+    // console.log(data.results[Math.floor(Math.random()*data.results.length)]);
+    setMovie(data.results[Math.floor(Math.random()*data.results.length)]);
+    }
+    // console.log(movie);
+    useEffect(()=>{
+      fetchData()
+    },[])
+
+  return (
+
+    <div className='banner' style={{
+        height:'600px',
+        backgroundSize:'cover',
+        backgroundImage:`url(${base_url}/${movie?.backdrop_path})`,
+        backgroundAttachment:'fixed'
+    }}>
+        <div className="banner_content" >
+            <h1>{movie?.name} </h1>
+            <h2>{movie?.overview?.slice(0,280)}... </h2>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Banner
